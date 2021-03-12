37a646d0c00e91329f9a84ae2f3c8a3c5b8e5cf3
Colby Fayock <fayock@gmail.com>
2021-03-11 22:58:08 -0500

    posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -84,11 +84,11 @@ export default function Home({ posts: defaultPosts }) {
 }
 
 export async function getStaticProps() {
-  const posts = await getAllPosts();
+  // const posts = await getAllPosts();
 
   return {
     props: {
-      posts
+      posts: []
     }
   }
 }
